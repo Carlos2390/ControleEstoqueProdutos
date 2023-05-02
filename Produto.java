6678f56f7bf769797be33b297f61816430e6798f
Carlos Matos <carlinhos12345678962@gmail.com>
2023-05-02 11:30:09 -0300

    Add files via upload

---
+++ Produto.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package DadosProdutos;
+
+/**
+ *
+ * @author Carlos Matos RGM: 29622182
+ */
+public class Produto {
+    public String nome;
+    public float preco;
+    public int qtdEstoque;
+    
+    public float totalValorEstoque;
+    
+    public Produto(String nome, float valor, int qtdEstoque){
+        this.nome = nome;
+        this.preco = valor;
+        this.qtdEstoque = qtdEstoque;
+    }
+    
+    public void CalculaValorEtoque(){
+        this.totalValorEstoque = this.qtdEstoque * this.preco;
+        System.out.println("Valor total no estoque: " + this.totalValorEstoque);
+    }
+    
+    public void AdicionaQuantidade(int qtdAdicionar){
+        this.qtdEstoque += qtdAdicionar;
+        System.out.println("Nome: "+ this.nome);
+        System.out.println("Preco: " + this.preco);
+        System.out.println("Quantidade no estoque: "+ this.qtdEstoque);
+        CalculaValorEtoque();
+    }
+    
+    public void RemoveQuantidade(int qtdRemover){
+        this.qtdEstoque -= qtdRemover;
+        System.out.println("Nome: "+ this.nome);
+        System.out.println("Preco: " + this.preco);
+        System.out.println("Quantidade no estoque: "+ this.qtdEstoque);
+        CalculaValorEtoque();
+    }
+    
+}
---
+++ TestaDadosProdutos.java
@@ -0,0 +1,45 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DadosProdutos;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Carlos Matos RGM: 29622182
+ */
+public class TestaDadosProdutos {
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        
+        System.out.println("Qual o nome do produto?");
+        String nome = entrada.nextLine();
+        
+        System.out.println("Qual o preco do produto?");
+        float preco = entrada.nextFloat();
+        
+        System.out.println("Qual a quantidade do produto no estoque?");
+        int qtdEstoque = entrada.nextInt();
+        
+        Produto p1 = new Produto(nome, preco, qtdEstoque);
+        
+        System.out.println("Deseja Adicionar ou Remover o produto do estoque?");
+        System.out.println("[1] Adicionar");
+        System.out.println("[2] Remover");
+        int escolha = entrada.nextInt();
+        
+        switch(escolha){
+            case 1:
+                System.out.println("Qual a quantidade a adicionar?");
+                int qtd = entrada.nextInt();
+                p1.AdicionaQuantidade(qtd);
+                break;
+            case 2:
+                System.out.println("Qual a quantidade a remover?");
+                qtd = entrada.nextInt();
+                p1.RemoveQuantidade(qtd);
+        }
+    }
+}
